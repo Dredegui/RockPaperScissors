78814287cb75760f19a86f47e052f6c50ece048b
dredegui <guilherme.m.pascoal@tecnico.ulisboa.pt>
2023-01-31 13:42:47 +0000

    Fix name inconsistency and add input error detection for choice

--- game.js
+++ game.js
@@ -5,13 +5,12 @@ function getComputerChoice () {
 	} else if (choice === 1) {
 		return "Paper";
 	} else {
-		return "Scissor"; 
+		return "Scissors"; 
 	}
 }
 
 function decide(choice1, choice2) {
-	if (!((choice1 === "Scissors" && choice2 == "Paper") || (choice1 === "Paper" && choice2 == "Rock") || 
-		(choice1 === "Rock" && choice2 == "Scissors"))) {
+	if (!((choice1 == "Scissors" && choice2 == "Paper") || (choice1 == "Paper" && choice2 == "Rock") || (choice1 == "Rock" && choice2 == "Scissors"))) {
 		return choice2;
 	}
 	return choice1;
@@ -19,7 +18,7 @@ function decide(choice1, choice2) {
 
 function round(playerSelection, computerSelection) {
 	let winner = "";
-	if (computerSelection === playerSelection) {
+	if (computerSelection == playerSelection) {
 		return "Tie!";
 	} else {
 		winner = decide(playerSelection, computerSelection);
@@ -37,9 +36,14 @@ function game(rounds) {
 	let computerSelection;
 	let result;
 	for(let i = 0; i < rounds; i++) {
-		playerSelection = prompt("Choose between Rock Paper and Scissor");
+		playerSelection = prompt("Choose between Rock Paper and Scissors");
 		playerSelection = playerSelection.toLowerCase();
 		playerSelection = playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase());
+		if (!(playerSelection.localeCompare("Rock") == 0 || playerSelection.localeCompare("Paper") == 0 || playerSelection.localeCompare("Scissors") == 0)) {
+			rounds++;
+			console.log("Invalid Awnser!");
+			continue;
+		}
 		computerSelection = getComputerChoice();
 		console.log(`Player chose: ${playerSelection}`);
 		console.log(`Computer chose: ${computerSelection}`);
