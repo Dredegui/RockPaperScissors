8969e91a47d4bff4be700ba3d6105151eddfc5d6
dredegui <guilherme.m.pascoal@tecnico.ulisboa.pt>
2023-02-04 19:47:32 +0000

    Create game UI

--- game.js
+++ game.js
@@ -31,26 +31,73 @@ function round(playerSelection, computerSelection) {
 	
 }
 
-function game(rounds) {
-	let playerSelection;
-	let computerSelection;
-	let result;
-	for(let i = 0; i < rounds; i++) {
-		playerSelection = prompt("Choose between Rock Paper and Scissors");
-		playerSelection = playerSelection.toLowerCase();
-		playerSelection = playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase());
-		if (!(playerSelection.localeCompare("Rock") == 0 || playerSelection.localeCompare("Paper") == 0 || playerSelection.localeCompare("Scissors") == 0)) {
-			rounds++;
-			console.log("Invalid Awnser!");
-			continue;
-		}
-		computerSelection = getComputerChoice();
-		console.log(`Player chose: ${playerSelection}`);
-		console.log(`Computer chose: ${computerSelection}`);
-		result = round(playerSelection, computerSelection);
-		console.log(result);
+function mkButton(usr, cl) {
+	let clname = cl.toLowerCase();
+	const button = document.createElement("button"); 
+	const div = document.createElement("div"); 
+	const who = document.createElement("h4"); 
+	button.className = `choice ${cl}`;
+	const img = document.createElement("img"); 
+	img.src = `img/${clname}-icon.png`;
+	button.appendChild(img);
+	who.textContent = usr;
+	who.style.margin = "0";
+	who.style.marginBottom = "2px";
+	who.style.textAlign = "center";
+	div.style.display = "flex";
+	div.style.justifyContent = "center";
+	div.style.flexDirection= "column";
+	div.appendChild(who);
+	div.appendChild(button);
+	return div;
+}
+
+function game(className) {
+	playerSelection = className.replace("choice ", "");
+	let computerSelection = getComputerChoice();
+	let playerButton = mkButton("Player", playerSelection);	
+	let computerButton = mkButton("Computer", computerSelection);
+	const para = document.createElement("p");
+	para.classList.add("choice");
+	para.textContent="vs";
+	para.style.textAlign = "center";
+	let par = document.querySelector(".choice-container");
+	let buttons = document.querySelectorAll(".choice-container button");
+	buttons.forEach((button) => par.removeChild(button));
+	par.appendChild(playerButton);
+	par.appendChild(para);
+	par.appendChild(computerButton);
+	const anc = document.createElement("h3");
+	anc.textContent = round(playerSelection, computerSelection);;
+	anc.style.textAlign = "center";
+	(par.parentNode).appendChild(anc);
+	if (anc.textContent.includes("win")) {
+		return 1;
+	} else {
+		return -1;
 	}
 }
 
-let rounds = prompt("How many rounds you want to play: ");
-game(rounds);
+function listenButtons() {
+	const buttons = document.querySelectorAll("button");
+	let score = 0;
+	buttons.forEach((button) => {
+		button.addEventListener('click', () => {
+			score += game(button.className);
+			let prmpt = document.querySelector(".prompt-container");
+			const plagin = document.createElement("button");
+			plagin.textContent = "Play Again";
+			prmpt.appendChild(plagin);
+			plagin.addEventListener("click", () => {
+				document.body.innerHTML = originalDOM;
+				listenButtons();
+			});
+		});
+	});
+	return;	
+}
+
+let originalDOM = document.body.innerHTML;
+console.log("wtf");
+listenButtons();
+// game();
