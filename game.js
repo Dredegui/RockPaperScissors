3c37ad20584d584985145b4ec292c74c64b1e4c9
dredegui <guilherme.m.pascoal@tecnico.ulisboa.pt>
2023-02-05 18:52:40 +0000

    Create specialized styles for buttons, and animation for computer choice

--- game.js
+++ game.js
@@ -33,13 +33,13 @@ function round(playerSelection, computerSelection) {
 
 function mkButton(usr, cl) {
 	let clname = cl.toLowerCase();
-	const button = document.createElement("button"); 
+	const imgDiv= document.createElement("div"); 
 	const div = document.createElement("div"); 
 	const who = document.createElement("h4"); 
-	button.className = `choice ${cl}`;
+	imgDiv.className = `choice ${cl}`;
 	const img = document.createElement("img"); 
 	img.src = `img/${clname}-icon.png`;
-	button.appendChild(img);
+	imgDiv.appendChild(img);
 	who.textContent = usr;
 	who.style.margin = "0";
 	who.style.marginBottom = "2px";
@@ -48,7 +48,51 @@ function mkButton(usr, cl) {
 	div.style.justifyContent = "center";
 	div.style.flexDirection= "column";
 	div.appendChild(who);
-	div.appendChild(button);
+	div.appendChild(imgDiv);
+	return div;
+}
+
+function animateRandom(div, image){
+	div.removeChild(image);
+	const divmoving = document.createElement("div"); 
+	const rock = document.createElement("img"); 
+	const paper = document.createElement("img"); 
+	const scissors = document.createElement("img"); 
+	rock.src = `img/rock-icon.png`;
+	paper.src = `img/paper-icon.png`;
+	scissors.src = `img/scissors-icon.png`;
+	div.appendChild(divmoving);
+	divmoving.appendChild(rock);
+	divmoving.appendChild(paper);
+	divmoving.appendChild(scissors);
+	divmoving.style.position = "absolute";
+	divmoving.style.top = "0";
+	let id = null;
+	let pos = 0;
+	clearInterval(id);
+	id = setInterval(frame, 1);
+	function frame() {
+		if (pos === -300) {
+			clearInterval(id);
+			div.removeChild(divmoving);
+			div.appendChild(image);
+		} else {
+			pos--;
+			divmoving.style.top = pos + "px";
+		}
+	}
+}
+
+
+
+function mkCenterDiv () {
+	let div = document.createElement("div");
+	div.style.display = "flex";
+	div.style.justifyContent = "center";
+	div.style.alignItems = "center";
+	div.style.flexDirection= "column";
+	div.classList.add("spec");
+	div.style.marginTop = "20px"
 	return div;
 }
 
@@ -58,15 +102,20 @@ function game(className) {
 	let playerButton = mkButton("Player", playerSelection);	
 	let computerButton = mkButton("Computer", computerSelection);
 	const para = document.createElement("p");
-	para.classList.add("choice");
 	para.textContent="vs";
 	para.style.textAlign = "center";
 	let par = document.querySelector(".choice-container");
 	let buttons = document.querySelectorAll(".choice-container button");
 	buttons.forEach((button) => par.removeChild(button));
+	let div = mkCenterDiv();
+	div.appendChild(para);
 	par.appendChild(playerButton);
-	par.appendChild(para);
+	par.appendChild(div);
 	par.appendChild(computerButton);
+	let corr = computerButton.lastElementChild.lastElementChild;
+	animateRandom(computerButton.lastElementChild,  computerButton.lastElementChild.lastElementChild);
+	let specid = null;
+	specid = setInterval()
 	const anc = document.createElement("h3");
 	anc.textContent = round(playerSelection, computerSelection);;
 	anc.style.textAlign = "center";
